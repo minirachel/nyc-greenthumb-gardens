401f1c004010a36cfca379c0eff8f382f61b44cd
minirachel <rachel.a.levinson@gmail.com>
2021-04-10 19:34:57 -0400

    gem install

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem 'pry'
+gem 'nokogiri', '1.10.8'
+gem 'rspec'
---
+++ lib/scraper.rb
@@ -0,0 +1,11 @@
+class Scraper
+
+    def scrape_map_hash
+        map = Nokogiri::HTML("https://nycfridge.com/")
+        binding.pry
+        fridges = {}
+
+        puts "hello"
+    end
+    
+end

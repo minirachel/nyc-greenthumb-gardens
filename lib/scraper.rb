6600bad6be5db7a0ddb81d7556b6d9457bcd8844
minirachel <rachel.a.levinson@gmail.com>
2021-04-10 20:35:07 -0400

    new files

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,27 @@
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+
 class Scraper
 
     def scrape_map_hash
-        map = Nokogiri::HTML("https://nycfridge.com/")
-        binding.pry
+        site = "https://nycfridge.com/"
+        map = Nokogiri::HTML(open(site))
+
         fridges = {}
+
+        map.css("div.pin").each do |pin|
+            
+            binding.pry
+        end
 
         puts "hello"
     end
-    
+
+    def math
+        equation = 2 + 2
+        equation
+    end
+
 end
+

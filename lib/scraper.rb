94ea33220b7b6b6e09ac08e422c7578b2a5e8092
minirachel <rachel.a.levinson@gmail.com>
2021-04-12 16:08:17 -0400

    census and httparty setup. API throttles without token

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,46 +1,48 @@
-require 'open-uri'
 require 'pry'
-require 'nokogiri'
+require 'httparty'
 
 class Scraper
 
-    def self.scrape_map_hash
-        # browser = Watir::Browser.new
-        # browser.goto("https://nycfridge.com/")
-        # js_doc = browser.element(css: "div.container_Container__X1m1k").wait_until(&:present?)
-        # map = Nokogiri::HTML(js_doc.inner_html)
-        # desired_results = map.css("div.container_Container__X1m1k").first
-        # browser.quit()
-        
-        site = open("https://www.nycgovparks.org/programs/wildlife-management/calendar/")
-        page = Nokogiri::HTML(site)
-        calendar = page.css("div.tab-content").first
+    def self.scrape_tree_census
+        site = 'https://data.cityofnewyork.us/resource/uvpi-gqnh.json'
+        page = HTTParty.get(site)
+        census = page.to_a
 
-        animals = []
-
-        calendar.css("div.tab-pane").each do |animal|
-            animals << {
-                :month => animal.css("h3").text,
-                :name => animal.css("h4").text,
-                :info => animal.css("p").text,
-            }
-            
-        animals
         binding.pry
-        end
 
+        info = []
+
+        # reports.css("div.tab-pane").each do |requests|
+        #     info << {
+
+        #     }
+        # end
+            
+        info
+    
     end
 
-    self.scrape_map_hash
-
-    def math
-        equation = 2 + 2
-        equation
-    end
+    self.scrape_tree_census 
 
 end
 
-# includes? "popupBubble".children.text == NAME AND ADDRESS
-# #(Attr:0x3ff84f21c730 {
-#     name = "class",
-#     value = "StyledBox-sc-13pk1d4-0 hcvciW Modal_popupBubble__1Z61H"
+    # def self.scrape_animal_calendar
+    #     #BORING parks idea but I tried nokogiri
+    #     site = open("https://www.nycgovparks.org/programs/wildlife-management/calendar/")
+    #     page = Nokogiri::HTML(site)
+    #     calendar = page.css("div.tab-content").first
+
+    #     animals = []
+
+    #     calendar.css("div.tab-pane").each do |animal|
+    #         animals << {
+    #             :month => animal.css("h3").text,
+    #             :name => animal.css("h4").text,
+    #             :info => animal.css("p").text,
+    #         }
+            
+    #     animals
+
+    #     end
+
+    # end
